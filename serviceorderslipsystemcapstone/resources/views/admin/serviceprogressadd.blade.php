8d4fdbe4832a3447cf28cac1bf0e8b771f1607af
melokb2023 <kyle.melo@lccdo.edu.ph>
2023-11-09 20:19:19 +0800

    nov92023

--- serviceorderslipsystemcapstone/resources/views/admin/serviceprogressadd.blade.php
+++ serviceorderslipsystemcapstone/resources/views/admin/serviceprogressadd.blade.php
@@ -3,8 +3,59 @@
   <div class="py-12" style="background-color:#CD5C5C;">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="background-color:#CD5C5C;">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="background-color:#CD5C5C;text-align:center">
-                <div class="p-6 text-gray-900 dark:text-gray-100" style="background-color:#C0C0C0;">
-                   <h6>Errors Encountered</h6>
+                <div class="p-6 text-gray-900 dark:text-gray-100" style="background-color:#CD5C5C;">
+                <style>
+body {font-family: Century;}
+* {box-sizing: border-box;}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+button[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+button[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button[type=submit]:hover {
+  background-color: #45a049;
+}
+
+
+
+
+</style>
+                   <h6 style="font-family: Century">Errors Encountered</h6>
                     @if($errors)
                        <ul>
                           @foreach($errors->all() as $error)
--- serviceorderslipsystemcapstone/resources/views/customer/startappointment.blade.php
+++ serviceorderslipsystemcapstone/resources/views/customer/startappointment.blade.php
@@ -43,7 +43,7 @@
 }
 
 </style>
-                <a class="button" href="{{ route('add-service') }}">
+                <a class="button" href="{{ route('add-appointment') }}">
                         Start Appointment
                   
 </x-app-layout>
